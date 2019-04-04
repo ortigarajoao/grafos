72942488442ea0e49fefacefb35290943f073514
Joao Ortigara <ortigara.joao@gmail.com>
2019-04-03 22:17:02 -0300

    Vertice #ifndef e troca para unordered_map

--- include/Vertice.h
+++ include/Vertice.h
@@ -1,14 +1,20 @@
+#ifndef VERTICE_H
+#define VERTICE_H
 
+#include <string>
+#include <unordered_map>
 
 class Vertice{
 
 private:
   std::string _rotulo;
-  std::vector<Vertice> _antecessores; //maybe map
-  std::vector<Vertice> _sucessores;  //maybe map
+  std::unordered_map<int,Vertice> _antecessores; //maybe map
+  std::unordered_map<int,Vertice> _sucessores;  //maybe map
 
 public:
   std::string rotulo();
-  std::vector<Vertice> antecessores();
-  std::vector<Vertice> sucessores();
+  std::unordered_map<int,Vertice> antecessores();
+  std::unordered_map<int,Vertice> sucessores();
 };
+
+#endif //VERTICE_H
